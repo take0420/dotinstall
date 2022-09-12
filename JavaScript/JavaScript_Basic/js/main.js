04cb5d68cd32a7ac1d1b1847f557b3f9dd5cd79c
take0420 <zakisen81@gmail.com>
2022-09-13 06:39:30 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('Hello world from main.js!');
+// console.log('Hello');
+// console.log("Hello");
+
+// 「!」は「''」の時には認識されない。「””」にする必要あり。
+// console.log('it's me!');
+console.log("it's me!");
+
+// console.log('Hello world from main.js!');
