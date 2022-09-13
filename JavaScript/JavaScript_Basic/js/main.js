c07e2fad8f06c523eb0a7d94ba107d934fa36ae2
take0420 <zakisen81@gmail.com>
2022-09-14 06:55:54 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,2 +1,14 @@
 'use stirct';
 
+// Hello と10回表示したい。
+// i は1ずつ値を増やしていくから let が用いられる。
+for (let i = 1; i <= 10; i++) {
+  // 「Hello10」と表示される。
+  // console.log('hello');
+
+  // Hello に番号が割り当てられて10回表示される。
+  // console.log('Hello' + i);
+  
+  // Hello に番号が割り当てられて10回表示される。
+  console.log(`Hello ${i}`);
+}
