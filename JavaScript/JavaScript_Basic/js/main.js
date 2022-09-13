50bfdb3fb1d30a92015fe4f5767be5531c2364e8
take0420 <zakisen81@gmail.com>
2022-09-14 06:47:51 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,18 +1,2 @@
-'use strict';
+'use stirct';
 
-const score = 60;
-const name = 'taguchi';
-
-// if (score >= 50) {
-//   if (name === 'taguchi') {
-//     console.log('Good job!')
-//   }
-// }
-
-// && なおかつ ( AND )
-// || もしくは ( OR )
-// !  〜ではない( NOT )
-
-if (score >= 50 && name === 'taguchi') {
-  console.log('Good job!');
-}
