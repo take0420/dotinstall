42a2d67e6bd3d51c47a0fbe6fda562c6ef814025
take0420 <zakisen81@gmail.com>
2022-09-15 07:04:22 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,22 +1,14 @@
 'use strict';
 
-// 配列の全ての要素に対して何らかの処理を行う方法
 {
-  const scores = [80, 90, 40, 70, 100];
+  const scores = [80, 90, 40, 70];
 
-// 要素を個々に取得
-  // console.log(`Score: ${scores[0]}` );
-  // console.log(`Score: ${scores[1]}` );
-  // console.log(`Score: ${scores[2]}` );
-
-// 要素の数が多くなっても使える方法
-// 0 から 3 未満を 1 ずつ増やしながら処理 ※ i = index
-  for (let i = 0; i < 3; i++) {
-  console.log(`Score: ${scores[i]}` );
-}
+  scores.push(60, 50)  // 要素の末尾に新要素を追加
+  scores.shift();  //要素の 0 番目を削除
+    
 
 for (let i = 0; i < scores.length; i++) {
-  console.log(`Score: ${scores[i]}` );
+  console.log(`Score: ${i} ${scores[i]}` );
 }
 
 }
