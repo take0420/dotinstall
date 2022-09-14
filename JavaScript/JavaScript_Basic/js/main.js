4e4d03670486d34533fd898499006dc7233cf4c9
take0420 <zakisen81@gmail.com>
2022-09-14 21:42:24 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -4,20 +4,21 @@
 // Adを1つ直すと全て直す必要があることに...
 // function showAd() {---}で定義すると、定義したものだけを直せば、他の処理も同時に変更されて便利。
 
-function showAd() {
+function showAd(message = 'Ad') { //仮引数
   console.log('----------')
-  console.log('----Ad----')
+  console.log(`---${message}---`)
   console.log('----------')
 }
 
+showAd('Header Ad'); //実引数
+console.log('Tom is great!')
+console.log('Tom is great!')
+
+// showAd('Ad');
 showAd();
 console.log('Tom is great!')
 console.log('Tom is great!')
 
-showAd();
-console.log('Tom is great!')
-console.log('Tom is great!')
-
-showAd();
+showAd('Footer Ad');
 console.log('Tom is great!')
 console.log('Tom is great!')
