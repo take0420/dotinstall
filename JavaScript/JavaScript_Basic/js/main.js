0d725f48c79695d2bdc2cbf80a038eb060d36a2d
take0420 <zakisen81@gmail.com>
2022-09-14 22:15:25 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,6 +1,11 @@
-'use stirct';
+'use strict';
 
-function f() {
+const x = 2;
+
+function f() {  // f = 1 理由；{}内で x = 1 と定義しているから。
   const x = 1;
   console.log(x);
 }
+
+f(); // f = 2 理由；const x = 2 と定義しているから。
+console.log(x);   
