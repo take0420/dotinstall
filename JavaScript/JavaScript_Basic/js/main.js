91e9103ca8574dea9086f804a05fdc892d6e344f
take0420 <zakisen81@gmail.com>
2022-09-14 22:08:10 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,24 +1,6 @@
 'use stirct';
 
-// ページの途中で広告(Ad)を挿入したい。
-// Adを1つ直すと全て直す必要があることに...
-// function showAd() {---}で定義すると、定義したものだけを直せば、他の処理も同時に変更されて便利。
-
-function showAd(message = 'Ad') { //仮引数
-  console.log('----------')
-  console.log(`---${message}---`)
-  console.log('----------')
+function f() {
+  const x = 1;
+  console.log(x);
 }
-
-showAd('Header Ad'); //実引数
-console.log('Tom is great!')
-console.log('Tom is great!')
-
-// showAd('Ad');
-showAd();
-console.log('Tom is great!')
-console.log('Tom is great!')
-
-showAd('Footer Ad');
-console.log('Tom is great!')
-console.log('Tom is great!')
