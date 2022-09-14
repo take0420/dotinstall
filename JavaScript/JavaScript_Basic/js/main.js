d7218c6c7eb4a7f794c4401c67185dec7fe9efb3
take0420 <zakisen81@gmail.com>
2022-09-15 07:13:16 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
 {
+  // 90点は間違いで40点と50点の2回分のスコアだと分かった
   const scores = [80, 90, 40, 70];
-
-  scores.push(60, 50)  // 要素の末尾に新要素を追加
-  scores.shift();  //要素の 0 番目を削除
-    
+  scores.splice(1, 1, 40, 50);
 
 for (let i = 0; i < scores.length; i++) {
   console.log(`Score: ${i} ${scores[i]}` );
