7258e9faf969e55dd87497054bbfa24f8c094842
take0420 <zakisen81@gmail.com>
2022-09-16 07:02:52 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,25 +1,15 @@
 'use strict';
 
 { 
-  const scores = [80, 90, 40, 70];    
+  const prices = [180, 190, 200];
+  
+// prices を 20円ずつ値上げ
+  // const updatePrices = prices.map((price) => {
+  //   return price + 20;
+  // });
 
-  // const [a, b, c, d] = scores;
-  // console.log(a);    
-  // console.log(b);   
-  // console.log(c);   
-  // console.log(d);    
-
-//配列にある先頭と２番目の要素を定数に入れ、残りは配列のまま表示
-  // const [a, b, ...others] = scores;
-  // console.log(a);    
-  // console.log(b);    
-  // console.log(others);
-
-//値の交換
-  let x = 30;
-  let y = 70;
-  [x, y] = [y, x];
-  console.log(x);
-  console.log(y);
-
+//省略Ver.
+  const updatePrices = prices.map(price => price + 20);
+  console.log(updatePrices);
 }
+
