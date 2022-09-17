2352db2b1a93cc418a15e15a39c1c8f968375a0a
take0420 <zakisen81@gmail.com>
2022-09-17 14:41:46 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -6,9 +6,19 @@
     y: 180,
   };
 
-const keys = Object.keys(point);
-keys.forEach(key => {
-console.log(`Key: ${key} Value: ${point[key]}`);
-});
+// const keys = Object.keys(point);
+// keys.forEach(key => {
+// console.log(`Key: ${key} Value: ${point[key]}`);
+// });
+
+//複数の座標を管理したかった時
+const points = [
+  {x: 30, y:20}, // 0番目
+  {x: 10, y:50}, // 1番目
+  {x: 40, y:40}, // 2番目
+];
+console.log(points[1].y); // 1番目の y を取り出す
 }   
+
+
 
