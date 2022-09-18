3b608c0617bf2f11ca7cfafddabad578a5d159d7
take0420 <zakisen81@gmail.com>
2022-09-18 09:01:00 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,24 +1,24 @@
 'use strict';
 
 {
-  const point = {
-    x: 100, 
-    y: 180,
-  };
+  // let x = 1;
+  // let y = x;
+  // x = 5;
+  // console.log(x); // 5 
+  // console.log(y); // 1
 
-// const keys = Object.keys(point);
-// keys.forEach(key => {
-// console.log(`Key: ${key} Value: ${point[key]}`);
-// });
+  // let x = [1, 2];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x); // [5, 2] 
+  // console.log(y); // [1, 2] ではなく [5, 2] になってしまう。 
+  // 理由;複雑なデータによってシステムに負荷がかからないよう簡略化して制御しているから。
 
-//複数の座標を管理したかった時
-const points = [
-  {x: 30, y:20}, // 0番目
-  {x: 10, y:50}, // 1番目
-  {x: 40, y:40}, // 2番目
-];
-console.log(points[1].y); // 1番目の y を取り出す
-}   
+  // では配列やオブジェクトの値を丸ごとコピーしたい場合はどうすればよいのか？
+  let x = [1, 2];
+  let y = [...x];
+  x[0] = 5;
+  console.log(x); // [5, 2] 
+  console.log(y); // [1, 2] 
 
-
-
+}
