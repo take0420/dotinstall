83b0717a686823627226b4af9312d25cacc9dc23
take0420 <zakisen81@gmail.com>
2022-09-18 15:10:35 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 {
-  const d = new Date();
-  // console.log(d) //標準日時
-
-  console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`);
+  const d = new Date(2019, 10); // 2019/11/01 00:00:00
+  d.setHours(10, 20, 30); //2019/11/01 10:20:30
+  d.setDate(31); //2019/12/01 10:20:30
+  d.setDate(d.getDate() + 3); //2019/12/04 10:20:30
+  console.log(d); 
 }
