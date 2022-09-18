48c9918ebcc41d2691cb27826a8805bc0e4fe1cc
take0420 <zakisen81@gmail.com>
2022-09-18 14:58:39 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,17 +1,7 @@
 'use strict';
 {
-  console.log(Math.random());
+  const d = new Date();
+  // console.log(d) //標準日時
 
-  // 0, 1, 2
-  // Math.floor(Math.random() * 3)
-  // 0, ..., n
-  // Math.floor(Math.random() * (n + 1))
-  // min, ..., max 
-  // Math.floor(Math.random() * (max + 1 - min)) + min
-
-  // 1 から 6までのランダムな整数を生成。
-  // 6 + 1 - 1
-  console.log(Math.floor(Math.random() * 6) + 1)  
-
-
+  console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`);
 }
