43147784d698c51f6077b6aa58547d5b83625920
take0420 <zakisen81@gmail.com>
2022-09-18 10:01:19 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,15 +1,25 @@
 'use strict';
 {
-  const d = [2019, 11, 14];
+  //数値の合計と平均を求めたい
+  const scores = [10, 3, 9];
 
-  console.log(d.join('/')); // 2019/11/14
-  console.log(d.join(''));// 20191114
+  let sum = 0
 
-  const t = '17:08:24';
-  console.log(t.split(':'));// '17', '08', '24'
-  
-  const [hour, minute, second] = t.split(':');
-  console.log(hour); // 17
-  console.log(minute); // 08
-  console.log(second); // 24
+  scores.forEach(score => {
+    sum += score;
+  });
+
+  const avg = sum / scores.length;
+
+  // console.log(sum); // 22
+  // console.log(avg); // 7.33333...
+
+  console.log(Math.floor(avg)); // 7 繰下げ
+  console.log(Math.ceil(avg)); // 8 繰上げ
+  console.log(Math.round(avg)); // 7 四捨五入
+  console.log(avg.toFixed(3)); // 7.333 小数点 3 桁まで表示
+
+  //乱数を生成 => ゲームによく使われる
+  // 0 以上 1 未満のランダムな数を表示
+  console.log(Math.random());
 }
