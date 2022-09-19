554960f57d8c787ce8e7774f8b6981aba1cebc56
take0420 <zakisen81@gmail.com>
2022-09-19 23:04:54 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,14 +1,15 @@
 'use strict';
 {
-//ユーザーから名前を受取り。それを大文字にする処理
-  // const name = 'taguchi';
-//本当は数値を入力するとエラーになるが、それでも実行したい場合 => 例外処理( try {} )をする。
+  const posts = [
+    {
+      text: 'JavaScriptの勉強中...',
+      likeCount: 0,
+    },
+    {
+      text: 'プログラミング楽しい！',
+      likeCount: 0,
+    },
+  ];
   
-  try {
-    console.log(name.toUpperCase());
-  } catch (e) {
-    console.log(e);
-  }
-//処理完了のメッセージ
-  console.log('Finish!');
+  
 }
