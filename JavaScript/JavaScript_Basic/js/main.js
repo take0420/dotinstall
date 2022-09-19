378bf9b5378f805cc6c71aa4c4b0046cc14c8b34
take0420 <zakisen81@gmail.com>
2022-09-19 22:53:07 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 {
-  let i = 0;
-
+  let i = 0
+  
   function showTime() {
     console.log(new Date());
-    // showTime が 3回実行された時、タイマーが停止
+    const timeoutId = setTimeout(showTime, 1000);
     i++;
-    if (i > 2) {  
-      clearInterval(intervalId);
+    if (i > 2) {
+      clearTimeout(timeoutId);
     }
   }
+// 1 秒後に 1回だけ実行
+  // setTimeout(showTime, 1000);
 
-  // 1000ミリ秒 = 1秒
-  const intervalId = setInterval(showTime, 1000);
-
+  showTime();
 }
