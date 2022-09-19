449dfdd962fb7b68e0233587863d35128fe3a409
take0420 <zakisen81@gmail.com>
2022-09-20 07:02:43 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -8,15 +8,21 @@
     }
 
     show() {
-     console.log(`${this.text} - ${this.likeCount}いいね`);
+     console.log(`${this.text} - ${this.likeCount} likes`);
     }
+
+     like() {
+      this.likeCount++;
+     } 
   }
-  
+
   const posts = [
     new Post('JavaScriptの勉強中...'),
     new Post('プログラミング楽しい！'),
   ];
   
+  posts[0].like();
+
   posts[0].show();
   posts[1].show();
 }
