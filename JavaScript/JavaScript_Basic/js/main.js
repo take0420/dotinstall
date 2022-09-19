b50abbb5209b11200a606a7213c9ceb438df6618
take0420 <zakisen81@gmail.com>
2022-09-20 06:55:50 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,28 +1,22 @@
 'use strict';
+
 {
-// 今、匿名で文章を投稿できる SNS を作っていると仮定。
-// 投稿が複数あるため、まずは配列を作り、その中に文章と「いいね」の数を管理するオブジェクトを複数生成。
+  class Post {
+    constructor (text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+     console.log(`${this.text} - ${this.likeCount}いいね`);
+    }
+  }
+  
   const posts = [
-    {
-
-      text: 'JavaScriptの勉強中...',
-      likeCount: 0, // いいね 0
-    //↑短縮Ver.
-     show() {
-      console.log(`${this.text} - ${this.likeCount}いいね`);
-   },
-    },
-
-    {
-      text: 'プログラミング楽しい！',
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-     },
-    },
+    new Post('JavaScriptの勉強中...'),
+    new Post('プログラミング楽しい！'),
   ];
-
-  // show(posts[0]);
+  
   posts[0].show();
   posts[1].show();
 }
