18f33100eb01111e1465b602d91ab6d06f52250b
take0420 <zakisen81@gmail.com>
2022-09-20 21:17:17 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,10 +1,45 @@
+// 'use strict';
+
+// {
+//   class Post {
+//     constructor (text) {
+//       this.text = text;
+//       this.likeCount = 0;
+//     }
+
+//     show() {
+//      console.log(`${this.text} - ${this.likeCount} likes`);
+//     }
+
+//      like() {
+//       this.likeCount++;
+//       this.show();
+//      } 
+
+//      // 静的メソッド => this は使えない。
+//      static showInfo() {
+//       console.log('Post class version 1.0');
+//      }
+//   }
+
+//   const posts = [
+//     new Post('JavaScriptの勉強中...'),
+//     new Post('プログラミング楽しい！'),
+//   ];
+  
+//   // posts[0].like();
+  
+//   Post.showInfo();
+// }
+
 'use strict';
 
 {
-  class Post {
-    constructor (text) {
+  class SponsoredPost {
+    constructor (text,sponsor) {
       this.text = text;
       this.likeCount = 0;
+      this.sponsor = sponsor;
     }
 
     show() {
@@ -15,6 +50,11 @@
       this.likeCount++;
       this.show();
      } 
+
+     // 静的メソッド => this は使えない。
+     static showInfo() {
+      console.log('Post class version 1.0');
+     }
   }
 
   const posts = [
@@ -22,8 +62,7 @@
     new Post('プログラミング楽しい！'),
   ];
   
-  posts[0].like();
-
-  // posts[0].show();
-  // posts[1].show();
+  // posts[0].like();
+  
+  Post.showInfo();
 }
