c2f15827a548429981d96b2a867207fa50342d12
take0420 <zakisen81@gmail.com>
2022-09-21 06:29:02 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,13 +1,9 @@
 'use strict';
 
 { 
-  // DOM は document という特殊なオブジェクトで扱うことができる。
-  // また、文書内から特定の要素を取得するには、 querySelector() というメソッドが有効。
   function update() {
-    // document.querySelector('h1').textContent = 'Changed!';
-    // document.querySelector('#target').textContent = 'Changed!';
     document.getElementById('target').textContent = 'Changed!';
   }
-  // リロードした 1 秒後に変化。
+  
   setTimeout(update, 1000)
 }
