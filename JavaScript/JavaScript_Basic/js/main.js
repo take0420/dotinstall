a8104761208510b00d263ead966d222a6c507130
take0420 <zakisen81@gmail.com>
2022-09-21 06:49:32 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -2,8 +2,17 @@
 
 { 
   function update() {
-    document.getElementById('target').textContent = 'Changed!';
+    // document.getElementById('target').textContent = 'Changed!';    
+  // querySelector は先頭の文字にしか効果がない。
+    // document.querySelector('p').textContent = 'Changed!';
+  // よって、querySelectorAll とする必要あり。
+    // document.querySelectorAll('p')[1].textContent = 'Changed!';
+  // 全ての要素を処理したい時は、forEach() で対応。
+    document.querySelectorAll('p').forEach((p, index) => {
+      p.textContent = `${index}番目のpです！`;
+    });
+    
   }
-  
+
   setTimeout(update, 1000)
 }
