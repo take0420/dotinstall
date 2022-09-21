7557bfb720cd7dd8699dd03c134ea96b2073a3b2
take0420 <zakisen81@gmail.com>
2022-09-22 07:16:20 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -2,17 +2,18 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const li = document.createElement('li');
-    // 色を選んでボタンを押したら下のリストに追加される機能。
-    const color = document.querySelector('select');
-    // li.textContentに値をセット。
-    // select の場合、value プロパティで選択された値と、
-    // selectedIndex プロパティで選択された値が何番目であったかを取得できる。
-    li.textContent = `${color.value} - ${color.selectedIndex}`;
-    // リストに追加。querySelector で ul を取得し、子要素の末尾に li を追加 
-    // => appendChild()を使う。
-    document.querySelector('ul').appendChild(li);
+    const colors = document.querySelectorAll('input');
+    let selectedColor;
+
+    colors.forEach(color => {
+      if (color.checked === true) {
+        selectedColor = color.value;
+      }
+    })
     
+    const li = document.createElement('li');
+    li.textContent = selectedColor;
+    document.querySelector('ul').appendChild(li);
   });
 }
 
