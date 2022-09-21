31d2d8120940463445fecbd18c0d3ccd2230358e
take0420 <zakisen81@gmail.com>
2022-09-21 22:31:48 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -2,11 +2,14 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    const item0 = document.querySelectorAll('li')[0];
+    // item0 のコピーを作る。
+    const copy = item0.cloneNode(true); // item2 の前に挿入したい。
 
-    // const ulNode = document.querySelector('ul');
+    // 新要素
     const ul = document.querySelector('ul');
-    ul.appendChild(item2);
+    const item2 = document.querySelectorAll('li')[2];
+    // copy を item2 の前に挿入してね。
+    ul.insertBefore(copy, item2);
   });
 }
