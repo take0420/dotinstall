3645c63bf31f7e1f70353fc4848669dfd7a69d57
take0420 <zakisen81@gmail.com>
2022-09-21 20:47:45 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -4,11 +4,21 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
     
-    targetNode.textContent = 'Changed!';
-    targetNode.title = 'This is title!';
-    targetNode.style.color = 'green';
-    targetNode.style.backgroundColor = 'skyblue';
-    
+    // targetNode.className = 'my-color my-border'; 
 
-    });
+  // 既存のクラスの設定に my-color を新たに追加。my-border を書く必要はない。
+    // targetNode.classList.add('my-color');
+
+// contains => クラスが付いているかどうか、true, false で返してくれる。
+// my-color が付いていたら外す classList.remove() 処理。
+    // if (targetNode.classList.contains('my-color') === true) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+
+    //省略Ver.
+    targetNode.classList.toggle('my-color');
+    
+  }); 
 }
