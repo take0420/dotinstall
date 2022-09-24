53cda686fa64e2ff101c4655301bdc7b9814fa98
take0420 <zakisen81@gmail.com>
2022-09-24 13:22:58 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
 {
-  document.querySelector('button').addEventListener('dblclick', () => {
-    console.log('Double Clicked!');
+  const text = document.querySelector('textarea');
+
+  text.addEventListener('focus', () => {
+    console.log('focus');
   });
 
-  // document.addEventListener('mousemove', e => {
-  //   // console.log('moved!');
-  //   console.log(e.clientX, e.clientY);
-
-// 適切に動いているかの確認方法 => 文書内をクリックして任意の文字を入力するとコンソールに反映される。
-  document.addEventListener('keydown', e => {
-    console.log(e.key);
-
+  text.addEventListener('blur', () => {
+    console.log('blur');
   });
+
+
 }
