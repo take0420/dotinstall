18bde4ae95f8b75387f8f9a31db88370bbf9a76a
take0420 <zakisen81@gmail.com>
2022-09-24 13:15:04 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -5,7 +5,13 @@
     console.log('Double Clicked!');
   });
 
-  document.addEventListener('mousemove', () => {
-    console.log('moved!');
+  // document.addEventListener('mousemove', e => {
+  //   // console.log('moved!');
+  //   console.log(e.clientX, e.clientY);
+
+// 適切に動いているかの確認方法 => 文書内をクリックして任意の文字を入力するとコンソールに反映される。
+  document.addEventListener('keydown', e => {
+    console.log(e.key);
+
   });
 }
