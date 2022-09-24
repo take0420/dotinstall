1af8b49b7c1dfd0305866b6a4cc3ab9e8b70e3fa
take0420 <zakisen81@gmail.com>
2022-09-24 13:28:16 +0900

    first commit

--- JavaScript/JavaScript_Basic/js/main.js
+++ JavaScript/JavaScript_Basic/js/main.js
@@ -3,13 +3,21 @@
 {
   const text = document.querySelector('textarea');
 
-  text.addEventListener('focus', () => {
-    console.log('focus');
+  // text.addEventListener('focus', () => {
+  //   console.log('focus');
+  // });
+
+  // text.addEventListener('blur', () => {
+  //   console.log('blur');
+  // });
+
+  text.addEventListener('input', () => {
+    // console.log('input');
+    console.log(text.value.length);
   });
 
-  text.addEventListener('blur', () => {
-    console.log('blur');
+  text.addEventListener('change', () => {
+    console.log('change');
   });
-
 
 }
