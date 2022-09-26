e13a019b6d196c7007de91aaaba1d9ac389ca9f6
take0420 <zakisen81@gmail.com>
2022-09-26 20:41:22 +0900

    first commit

--- JavaScript/My_StopWatch/js/main.js
+++ JavaScript/My_StopWatch/js/main.js
@@ -3,6 +3,22 @@
 {
   const timer = document.getElementById('timer');
   const start = document.getElementById('start');
-  const stop= document.getElementById('stop');
+  const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+  // カウントアップ
+  function countUp() {
+    console.log(Date.now() - startTime);
+
+    // countUp()の処理を一定時間 (10ミリ秒後に)ごとに繰り返したい。
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
 }
