edb31c892f3c64d5f2997cebce81b9f12b73f6b3
take0420 <zakisen81@gmail.com>
2022-09-26 21:24:26 +0900

    first commit

--- JavaScript/My_StopWatch/js/main.js
+++ JavaScript/My_StopWatch/js/main.js
@@ -8,11 +8,12 @@
 
   let startTime;
   let timeoutId;
+  let elapsedTime = 0; // タイマー停止からカウントを再開させる機能
 
   // カウントアップ
   function countUp() {
     // 経過時間で Date オブジェクトを作成。そのメソッドを使用する。
-    const d = new Date(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime + elapsedTime);
     // 値を２桁で表示してね。２桁に未満なら文字列の前を０で埋めてね。
     const m = String(d.getMinutes()).padStart(2, '0');
     const s = String(d.getSeconds()).padStart(2, '0');
@@ -25,16 +26,44 @@
     }, 10);
   }
 
+  // スタートの状態で有効(disabled = false )なボタン
+  function setButtonStateInitial() {
+    start.disabled = false; // 有効
+    stop.disabled = true; // 無効
+    reset.disabled = true; // 無効
+
+  }
+  // カウントアップの状態で有効(disabled = false )なボタン
+  function setButtonStateRunning() {
+    start.disabled = true; // 無効
+    stop.disabled = false; // 有効
+    reset.disabled = true; // 無効
+
+  }
+  // ストップの状態で有効(disabled = false )なボタン
+  function setButtonStateStopped()  {
+    start.disabled = false; // 有効
+    stop.disabled = true; // 無効
+    reset.disabled = false; // 有効
+  }
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime; 
   });
 
   reset.addEventListener('click', () => {
+    setButtonStateInitial();
     timer.textContent = '00:00.000';
+    elapsedTime = 0;
   });
 }
