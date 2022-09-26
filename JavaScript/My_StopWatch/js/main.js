7e9727a552a2304d82bd92537c3813bee968cb07
take0420 <zakisen81@gmail.com>
2022-09-26 20:53:28 +0900

    first commit

--- JavaScript/My_StopWatch/js/main.js
+++ JavaScript/My_StopWatch/js/main.js
@@ -9,7 +9,13 @@
   let startTime;
   // カウントアップ
   function countUp() {
-    console.log(Date.now() - startTime);
+    // 経過時間で Date オブジェクトを作成。そのメソッドを使用する。
+    const d = new Date(Date.now() - startTime);
+    // 値を２桁で表示してね。２桁に未満なら文字列の前を０で埋めてね。
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
 
     // countUp()の処理を一定時間 (10ミリ秒後に)ごとに繰り返したい。
     setTimeout(() => {
