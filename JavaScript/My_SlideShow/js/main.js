ad2927a43c4fc0a96dd0db391e16e9413e0e735b
take0420 <zakisen81@gmail.com>
2022-09-27 21:52:49 +0900

    first commit

--- JavaScript/My_SlideShow/js/main.js
+++ JavaScript/My_SlideShow/js/main.js
@@ -26,23 +26,24 @@
     }
     li.addEventListener('click', () => {
       mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
     });
 
-      li.appendChild(img);
+    li.appendChild(img);
     document.querySelector('.thumbnails').appendChild(li);
   });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+});
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
