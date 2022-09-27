c408ecef6cc3237e241fdd4efc5692dd28df9b76
take0420 <zakisen81@gmail.com>
2022-09-27 21:30:55 +0900

    first commit

--- JavaScript/My_SlideShow/js/main.js
+++ JavaScript/My_SlideShow/js/main.js
@@ -16,32 +16,22 @@
   const mainImage = document.getElementById('main');
   mainImage.src = images[currentIndex];
 
-  images.forEach(image => {
+  images.forEach((image, index) => {
     const img = document.createElement('img');
     img.src = image;
 
     const li = document.createElement('li');
-    li.appendChild(img);
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+    });
+
+      li.appendChild(img);
     document.querySelector('.thumbnails').appendChild(li);
   });
 }
-
-
-// メイン画像と同じように JavaScript でセット。
-
-// images の配列の要素の数の分だけループを回したいので forEach() を使ってあげます。
-
-// images のそれぞれの要素を image で取得してあげて、次の処理をしなさい。
-
-// まずは img 要素を生成する必要があるので、 img という定数で document.createElement('img') としてあげましょう。
-
-// その img の src 属性をこちらの image になるようにしてあげます。
-
-// li の子要素として、こちらの img を追加していきたいので appendChild() を使ってあげます。
-
-// 最後にこちらの thumbnails クラスがついた ul の子要素として li を追加してあげれば OK でしょう。
-
-// これで images の配列の要素数分だけ追加される。
 
 
 
