8082b10297e0d0a461ce537b17ac43334237836e
take0420 <zakisen81@gmail.com>
2022-09-28 20:58:49 +0900

    first commit

--- JavaScript/My_NumbersGame/js/main.js
+++ JavaScript/My_NumbersGame/js/main.js
@@ -6,16 +6,28 @@
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+
+    getEl() {
+      return this.el;
+    }
   }
+
   class Board {
     constructor() {
       this.panels = [];
-      for (let i = 0; i < 4; i++){
+      for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel());
       }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl()); 
+      });
     }
   }
 
   const board = new Board();
 }
-
