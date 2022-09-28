bbd2ad704e764e405580301d9b0e7874cdaca3ac
take0420 <zakisen81@gmail.com>
2022-09-28 22:25:14 +0900

    first commit

--- JavaScript/My_NumbersGame/js/main.js
+++ JavaScript/My_NumbersGame/js/main.js
@@ -25,7 +25,7 @@
         this.el.classList.add('pressed');
         this.game.addCurrentNum();
 
-        if (this.game.getCurrentNum() === 4) {
+        if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
           clearTimeout(this.game.getTimeoutId());
         }
       }
@@ -34,10 +34,10 @@
 
   class Board {
     constructor(game) {
-      this.game = game
+      this.game = game;
       this.panels = [];
-      for (let i = 0; i < 4; i++) {
-        this.panels.push(new Panel());
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        this.panels.push(new Panel(this.game));
       }
       this.setup();
     }
@@ -50,7 +50,10 @@
     }
 
     activate() {
-      const nums = [0, 1, 2, 3];
+      const nums = [];
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        nums.push(i);
+      }
 
       this.panels.forEach(panel => {
         const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
@@ -60,7 +63,8 @@
   }
 
   class Game {
-    constructor() {
+    constructor(level) {
+      this.level = level;
       this.board = new Board(this);
 
       this.currentNum = undefined;
@@ -71,6 +75,15 @@
       btn.addEventListener('click', () => {
         this.start();
       });
+      this.setup();
+    }
+
+    setup() {
+      const container = document.getElementById('container');
+      const PANEL_WIDTH = 50;
+      const BOARD_PADDING = 10;
+      /* 50px * 2 + 10px * 2 */
+      container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
     }
 
     start() {
@@ -106,15 +119,10 @@
       return this.timeoutId;
     }
 
-
+    getLevel() {
+      return this.level;
+    }
   }
 
-  new Game();
+  new Game(5);
 }
-
-
-
-
-
-
-
